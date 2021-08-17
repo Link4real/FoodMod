1869a60c0dcf5cbc67138fb56e24e754aa4e9224
Link4Real <simon@die-duerrs.de>
2021-08-17 12:04:52 +0200

    Ava

--- src/main/java/com/link/food/mod/registry/ModItems.java
+++ src/main/java/com/link/food/mod/registry/ModItems.java
@@ -11,7 +11,6 @@ import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
-
     public static final Item MANGO = new Item(new Item.Settings().group(ItemGroup.FOOD).maxCount(64).food(new FoodComponent.Builder().hunger(3).saturationModifier(5f).build()));
     public static final Item SALAD = new Item(new Item.Settings().group(ItemGroup.FOOD).maxCount(16).food(new FoodComponent.Builder().hunger(5).saturationModifier(2f).build()));
     public static final Item BURGER = new Item(new Item.Settings().group(ItemGroup.FOOD).maxCount(16).food(new FoodComponent.Builder().hunger(8).saturationModifier(7f).build()));
@@ -58,5 +57,6 @@ public class ModItems {
         Registry.register(Registry.ITEM, new Identifier("foodmod", "tortilla"), TORTILLA);
         Registry.register(Registry.ITEM, new Identifier("foodmod", "taco_shell"), TACO_SHELL);
         Registry.register(Registry.ITEM, new Identifier("foodmod", "salsa"), SALSA);
+
     }
 }
